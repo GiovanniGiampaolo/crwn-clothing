0ec292d6322aae22b7ba88b2c6bc40ffab21f604
Giovanni <giovannigiampaolo58@gmail.com>
2019-08-27 16:20:37 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import HomepageComponent from './homepage.component'
+
+function App() {
+    return (
+        <div>
+            <HomepageComponent/>
+        </div>
+    )
+}
+
+export default App
---
+++ src/homepage.component.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+export const HomepageComponent = () => {
+
+    return(
+
+        <div className="Homepage">
+
+            {/* MENU */}
+            <div className="directory-menu"></div>
+
+            {/* MENU ITEM */}
+            <div className="menu-item">
+                <div className="content">
+                    <h1>title</h1>
+                    <span>Subtitle</span>
+                </div>
+
+                <div className="content">
+                    <h1>title2</h1>
+                    <span>Subtitle2</span>
+                </div>
+
+                <div className="content">
+                    <h1>title</h1>
+                    <span>Subtitle</span>
+                </div>
+
+                <div className="content">
+                    <h1>title2</h1>
+                    <span>Subtitle2</span>
+                </div>
+
+                <div className="content">
+                    <h1>title2</h1>
+                    <span>Subtitle2</span>
+                </div>
+
+
+            </div>
+        </div>)
+}
+
+export default HomepageComponent
+
