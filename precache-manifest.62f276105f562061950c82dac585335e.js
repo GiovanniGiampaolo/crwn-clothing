a0739647bdaaeb8fd10835720ed45bb7c49080ed
Giovanni <giovannigiampaolo58@gmail.com>
2019-08-29 16:49:45 +0200

    Updates

---
+++ precache-manifest.62f276105f562061950c82dac585335e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf38da25a34dd32cc6e1cd49f87f9215",
+    "url": "/crwn-clothing/index.html"
+  },
+  {
+    "revision": "6c3529a9c192988565e7",
+    "url": "/crwn-clothing/static/css/main.80590799.chunk.css"
+  },
+  {
+    "revision": "dbe28a8d27e1b3691ab3",
+    "url": "/crwn-clothing/static/js/2.e1ac2598.chunk.js"
+  },
+  {
+    "revision": "6c3529a9c192988565e7",
+    "url": "/crwn-clothing/static/js/main.3b2083cd.chunk.js"
+  },
+  {
+    "revision": "ddc729574f128ccd2719",
+    "url": "/crwn-clothing/static/js/runtime~main.20c21b29.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/crwn-clothing/static/media/crown.svg.ed28accb.svg"
+  }
+]);
